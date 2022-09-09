b4db0cfdfb01754cb3b3ad96182c34774e5b4a6c
Chukwuma Ike <chukwumaike208@gmail.com>
2022-09-08 17:11:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print the string in the print function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzl
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
@@ -12,13 +12,13 @@ int main(void)
 	char c;
 	int i;
 	long li;
-	long long lli;
+	long long d;
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d bytes(s)\n", sizeof(f));
 	return (0);
 }
