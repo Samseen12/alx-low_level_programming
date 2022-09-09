d140f19c8168d03f60fd204d1d9afb4aaae06db6
Chukwuma Ike <chukwumaike208@gmail.com>
2022-09-08 17:18:39 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the print function
+ * main - print the sizeof many var types
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
+	char a;
+	int b;
+	long int c;
 	long long d;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d bytes(s)\n", sizeof(f));
 	return (0);
