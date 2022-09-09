6fced7eb11c4f0c321bb40b618f7e508eb2df718
Chukwuma Ike <chukwumaike208@gmail.com>
2022-09-08 17:08:04 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print the string in the print function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzl
  * Return: 0
  */
 int main(void)
